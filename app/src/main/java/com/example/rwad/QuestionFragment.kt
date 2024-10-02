6695d63a0d564a8b43c3b758a7af6f40fab468af
Ahmed Allam <3lam.ahmedd@gmail.com>
2024-10-02 19:41:52 +0300

    Quiz App(2)

--- app/src/main/java/com/example/rwad/QuestionFragment.kt
+++ app/src/main/java/com/example/rwad/QuestionFragment.kt
@@ -1,10 +1,10 @@
 package com.example.rwad
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.Navigation
@@ -12,45 +12,32 @@ import com.example.rwad.databinding.FragmentQuestionBinding
 import com.example.rwad.viewModel.QuestionsViewModel
 
 class QuestionFragment : Fragment() {
-lateinit var binding: FragmentQuestionBinding
- val viewModel : QuestionsViewModel by viewModels()
-    lateinit var myAnswer:String
+    lateinit var binding: FragmentQuestionBinding
+    val viewModel: QuestionsViewModel by viewModels()
+    lateinit var myAnswer: String
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
+
         binding = FragmentQuestionBinding.inflate(inflater, container, false)
-        binding.questionTitle.text =viewModel.questions[viewModel.index.value!!].title
-        binding.radioButton1.text =viewModel.questions[viewModel.index.value!!].answers[0]
-        binding.radioButton2.text =viewModel.questions[viewModel.index.value!!].answers[1]
-        binding.radioButton3.text =viewModel.questions[viewModel.index.value!!].answers[2]
-        binding.radioButton4.text =viewModel.questions[viewModel.index.value!!].answers[3]
-        binding.scoreTextView.text = "Score: ${viewModel.score}"
+        binding.questionViewModel = viewModel
+        binding.lifecycleOwner = this
 
-        viewModel.index.observe(viewLifecycleOwner, Observer{ newIndex ->
-            binding.questionTitle.text =viewModel.questions[ newIndex].title
-            binding.radioButton1.text =viewModel.questions[ newIndex].answers[0]
-            binding.radioButton2.text =viewModel.questions[ newIndex].answers[1]
-            binding.radioButton3.text =viewModel.questions[ newIndex].answers[2]
-            binding.radioButton4.text =viewModel.questions[ newIndex].answers[3]
-
-        })
-
-        viewModel.score.observe(viewLifecycleOwner, Observer{ newScore ->
-            binding.scoreTextView.text = "Score: $newScore"
-        })
-
-
-        binding.nextButton.setOnClickListener{
-            myAnswer = when(binding.radioGroup.checkedRadioButtonId){
+        binding.nextButton.setOnClickListener {
+            myAnswer = when (binding.radioGroup.checkedRadioButtonId) {
                 R.id.radioButton1 -> binding.radioButton1.text.toString()
                 R.id.radioButton2 -> binding.radioButton2.text.toString()
                 R.id.radioButton3 -> binding.radioButton3.text.toString()
                 R.id.radioButton4 -> binding.radioButton4.text.toString()
                 else -> ""
             }
-            if(myAnswer.isNotEmpty())
-           viewModel.nextQuestion(myAnswer)
+            if (myAnswer.isNotEmpty()) viewModel.nextQuestion(myAnswer, navigateToSuccess = {
+                Navigation.findNavController(it).navigate(R.id.action_questionFragment_to_successFragment)
+            },
+            navigateToFailure = {
+                Navigation.findNavController(it).navigate(R.id.action_questionFragment_to_failureFragment)
+            }
+            )
 
         }
 
